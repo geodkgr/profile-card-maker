da63c258018cb3c1540d6b10442f0efa3cf9de73
geodkgr <99806511+geodkgr@users.noreply.github.com>
2022-05-14 16:32:18 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const btn = document.querySelector("#btn");
+
+btn.addEventListener("click", showResults)
+
+var resultCard = document.getElementById("resultCard");
+var resName = document.getElementById("nameResult");
+var resLastName = document.getElementById("lastNameResult");
+var resBornDate = document.getElementById("bornDateResult");
+
+function showResults() {
+    // Input Values
+    var name = document.getElementById("inputName") .value;
+    var lastName = document.getElementById("inputLastName") .value;
+    var bornDate = document.getElementById("inputBornDate") .value;
+
+    resName.innerHTML = name;
+    resLastName.innerHTML = lastName;
+    resBornDate.innerHTML = bornDate;
+
+    // Alert
+    alert("20%");
+    alert("40%");
+    alert("97%");
+    alert("99%");
+    alert("100% ! Press OK to see the result");
+
+    resultCard.style.visibility = "visible";
+
+}
